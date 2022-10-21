958d900569bb902639741d94ea197699e4e1fe12
John Mark Ballangca <jmballangca001@gmail.com>
2022-10-22 04:52:02 +0800

    starter

---
+++ src/Application.tsx
@@ -0,0 +1,13 @@
+import LandingPage from "./components/Landing";
+
+interface IApplicationProps {}
+
+const Application: React.FunctionComponent<IApplicationProps> = (props) => {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+};
+
+export default Application;
---
+++ src/components/Landing.tsx
@@ -0,0 +1,14 @@
+
+
+interface ILandingPageProps { }
+
+const LandingPage: React.FunctionComponent<ILandingPageProps> = (props) => {
+  return (
+      <>
+
+      <h1>Your Code Here!</h1>
+    </>
+  );
+};
+
+export default LandingPage;
